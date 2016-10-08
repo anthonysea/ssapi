828562fc4c9c3046f65947a95104d9d6309268d2
Si Quick <siquick@gmail.com>
2016-10-08 20:37:37 +0700

    algo update

--- api/api.py
+++ api/api.py
@@ -288,7 +288,7 @@ GROUP by artist
 ORDER BY cnt DESC"""
  
 	#get the similar artists that appear more than once
-	getRecs = db_select(sql,(userName,userName,userName,userName))
+	getRecs = db_select(sql,(userName,userName,userName,userName,userName,userName))
 
 	dataArtists = getRecs.fetchall()
 	for artistRow in dataArtists:

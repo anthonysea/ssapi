e39e270c9ae805a63c262f7a7dd5b89887c524d7
Si Quick <siquick@gmail.com>
2017-06-28 20:21:22 +1000

    upped to 70

--- api/api.py
+++ api/api.py
@@ -437,7 +437,7 @@ def update_recommendations(api_key,user):
 	dataReleases = getReleases.fetchall()
 	count = 0
 	
-	dataReleases = dataReleases[0:40] #this gives us the first 70 releases which is what we want
+	dataReleases = dataReleases[0:70] #this gives us the first 70 releases which is what we want
 
 	for releasesRow in dataReleases:
 			

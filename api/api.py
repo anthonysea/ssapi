b654d84a1b9d99394ed09dfbf8db71dba300c3e2
Si Quick <siquick@gmail.com>
2017-07-15 23:31:46 +1000

    algo

--- api/api.py
+++ api/api.py
@@ -455,7 +455,7 @@ def update_recommendations(api_key,user):
 	dataReleases = getReleases.fetchall()
 	count = 0
 	
-	dataReleases = dataReleases[0:90] #this gives us the first 70 releases which is what we want
+	dataReleases = dataReleases[0:150] #this gives us the first 70 releases which is what we want
 
 	for releasesRow in dataReleases:
 			
@@ -502,7 +502,7 @@ def update_recommendations(api_key,user):
 	dataReleases = getReleases.fetchall()
 	count =0
 
-	dataReleases = dataReleases[0:90] #this gives us the first 10 releases which is what we want
+	dataReleases = dataReleases[0:150] #this gives us the first 10 releases which is what we want
 	
 	for releasesRow in dataReleases:
 

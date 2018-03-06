3f85fa9f8b1f122a2a308b1983969b0e4b020850
Si Quick <siquick@gmail.com>
2018-03-06 17:21:33 +1100

    added Rush Hour

--- api/scrapes.py
+++ api/scrapes.py
@@ -4,6 +4,7 @@ from flask import Flask, request, render_template, jsonify,abort, make_response
 import config
 import decimal
 import json
+import re
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -137,7 +138,34 @@ def get_hard_wax_release(api_key,hardwax_id):
     else:
         return jsonify({'store':'hardwax','in_stock':'true','cart_url':cart_url})
 
+
+
+@app.route('/api/v1.0/<int:api_key>/scrape/rushhour/release/<string:rushhour_id>',methods=['GET'])
+def get_rushhour_release(api_key,rushhour_id):
+    if str(api_key)!=the_api_key:
+        return 401
+    base_url = 'http://www.rushhour.nl/store_detailed.php?item=' + rushhour_id
+    ####now get the HTML
+    try:
+        r = requests.get(base_url)
+    except Exception as e:
+        return "Failed to request the Rush Hour URL " + base_url, 405
+
+    soup = BeautifulSoup(r.text, "lxml")
+    stock_details = soup.findAll("img",class_="cart_icon")
+    print(stock_details)
+
+    cart_url = 'http://www.rushhour.nl/store_detailed.php?action=add&item=' + rushhour_id
+
+    if len(stock_details) > 0:
+        return jsonify({'store':'rushhour','in_stock':'true','cart_url':cart_url})
+    else:
+        return jsonify({'store':'rushhour','in_stock':'false','cart_url':cart_url})
+
     
+
+    
+
 ######scrapes everything on a page
 def get_hard_wax(base_url):
 
@@ -196,5 +224,87 @@ def get_hard_wax(base_url):
         
         
     return base_url,201
+
+
+@app.route('/api/v1.0/<int:api_key>/scrape/rushhour/all',methods=['GET'])
+########scrapes the Rush Hour index page
+def get_rush_hour_index(api_key):
+    if str(api_key)!=the_api_key:
+        return 401
+    base_url = 'http://www.rushhour.nl/store_master.php?idxGroup=2&idxGenre=2&idxSubGenre=&app=250'
+
+    ####now get the HTML
+    try:
+        r = requests.get(base_url)
+    except Exception as e:
+        return "Failed to request the Rush Hour URL " + base_url, 405
+
+    soup = BeautifulSoup(r.text, "lxml")
+
+    
+
+    for product in soup.find_all("div","item_wrap1"):
+        details = str()
+        label_html = str()
+        label = str()
+        label_url = str()
+        artist_title = str()
+        split_a_t = str()
+        artist = str()
+        title = str()
+        release_url = str()
+
+        
+
+        the_release = product.find("div","item_content")
+        
+        all_details = the_release.find("h2","title")
+        #print all_details
+        release_url = all_details.findAll("a")[0]['href']
+        url_split = release_url.split('=')
+        store_release_id = url_split[1]
+        print store_release_id
+        all_details_reg = all_details.text.split(' - ')
+        title = all_details_reg[1]
+        label = all_details_reg[2]
+        print title,label
+
+
+        if len(store_release_id)<1:
+            print('Didnt get the store id - skip')
+            continue
+
+        if len(label) < 3 or len(title) < 3:
+            print('skipping ' + title + ' or ' + label + ' as less than 3 characters')
+            continue
+
+        
+        #sql = ('SELECT id FROM releases_all WHERE label_no_country LIKE %s AND title LIKE %s') % ('%' + label + '%','%' + title + '%')
+        try:
+            query = db_insert('INSERT INTO store_mappings (release_id,store,store_url,unique_key,store_release_id) SELECT id,%s,%s, md5(concat(id,%s)),%s FROM releases_all WHERE label_no_country LIKE %s AND title LIKE %s ON DUPLICATE KEY UPDATE store_url=values(store_url),store_release_id=values(store_release_id)', ('rushhour','/' + release_url,'rushhour',store_release_id,label + '%','%' + title + '%'))
+            data = query.fetchall()
+            print(query,data)
+        except Exception as e:
+            print(str(e))
+            continue
+        
+
+
+
+        
+
+    return base_url,201
+
+
+
+
+
+
+
+
+
+
+
+
 
 

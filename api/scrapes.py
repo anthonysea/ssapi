1da47c1f18c59b3ec3174dbf7c320ee013d52177
Si Quick <siquick@gmail.com>
2018-03-06 17:40:02 +1100

    minor

--- api/scrapes.py
+++ api/scrapes.py
@@ -209,7 +209,7 @@ def get_hard_wax(base_url):
 def get_rush_hour_index(api_key):
     if str(api_key)!=the_api_key:
         return 401
-    base_url = 'http://www.rushhour.nl/store_master.php?idxGroup=2&idxGenre=2&idxSubGenre=&app=250'
+    base_url = 'http://www.rushhour.nl/store_master.php?idxGroup=2&idxGenre=2&idxSubGenre=&app=1'
 
     ####now get the HTML
     try:

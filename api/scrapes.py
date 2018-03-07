857919183b529ee2f8aac83b776a552f576c8a65
Si Quick <siquick@gmail.com>
2018-03-08 07:42:16 +1100

    minor

--- api/scrapes.py
+++ api/scrapes.py
@@ -219,69 +219,69 @@ def get_rush_hour_index(api_key):
         return "Failed to request the Rush Hour URL " + base_url, 405
 
     #need to use selenium because of the popup
-    # browser = webdriver.Firefox()
-    # browser.get(base_url)
-    # try:
-    #     alert = browser.switch_to_alert()
-    #     alert.accept()
-    #     print "alert accpted"
-    # except:
-    #     print "no alert"
-    # html = browser.page_source
-    # browser.close()
+    browser = webdriver.Firefox(log_path="/var/www/ssapi/logs/geckodriver.log")
+    browser.get(base_url)
+    try:
+        alert = browser.switch_to_alert()
+        alert.accept()
+        print "alert accpted"
+    except:
+        print "no alert"
+    html = browser.page_source
+    browser.close()
 
    
 
-    soup = BeautifulSoup(r.text, "lxml")
+    soup = BeautifulSoup(html, "lxml")
 
     return soup.text
 
     
 
-    # for product in soup.find_all("div","item_wrap1"):
-    #     details = str()
-    #     label_html = str()
-    #     label = str()
-    #     label_url = str()
-    #     artist_title = str()
-    #     split_a_t = str()
-    #     artist = str()
-    #     title = str()
-    #     release_url = str()
+    for product in soup.find_all("div","item_wrap1"):
+        details = str()
+        label_html = str()
+        label = str()
+        label_url = str()
+        artist_title = str()
+        split_a_t = str()
+        artist = str()
+        title = str()
+        release_url = str()
 
         
 
-    #     the_release = product.find("div","item_content")
+        the_release = product.find("div","item_content")
         
-    #     all_details = the_release.find("h2","title")
-    #     #print all_details
-    #     release_url = all_details.findAll("a")[0]['href']
-    #     url_split = release_url.split('=')
-    #     store_release_id = url_split[1]
-    #     print store_release_id
-    #     all_details_reg = all_details.text.split(' - ')
-    #     title = all_details_reg[1]
-    #     label = all_details_reg[2]
-    #     print title,label
+        all_details = the_release.find("h2","title")
+        #print all_details
+        release_url = all_details.findAll("a")[0]['href']
+        url_split = release_url.split('=')
+        store_release_id = url_split[1]
+        print store_release_id
+        all_details_reg = all_details.text.split(' - ')
+        title = all_details_reg[1]
+        label = all_details_reg[2]
+        print title,label
 
 
-    #     if len(store_release_id)<1:
-    #         print('Didnt get the store id - skip')
-    #         continue
+        if len(store_release_id)<1:
+            print('Didnt get the store id - skip')
+            continue
 
-    #     if len(label) < 3 or len(title) < 3:
-    #         print('skipping ' + title + ' or ' + label + ' as less than 3 characters')
-    #         continue
+        if len(label) < 3 or len(title) < 3:
+            print('skipping ' + title + ' or ' + label + ' as less than 3 characters')
+            continue
 
         
-    #     #sql = ('SELECT id FROM releases_all WHERE label_no_country LIKE %s AND title LIKE %s') % ('%' + label + '%','%' + title + '%')
-    #     try:
-    #         query = db_insert('INSERT INTO store_mappings (release_id,store,store_url,unique_key,store_release_id) SELECT id,%s,%s, md5(concat(id,%s)),%s FROM releases_all WHERE label_no_country LIKE %s AND title LIKE %s ON DUPLICATE KEY UPDATE store_url=values(store_url),store_release_id=values(store_release_id)', ('rushhour','/' + release_url,'rushhour',store_release_id,label + '%','%' + title + '%'))
-    #         data = query.fetchall()
-    #         print(query,data)
-    #     except Exception as e:
-    #         print(str(e))
-    #         continue
+        #sql = ('SELECT id FROM releases_all WHERE label_no_country LIKE %s AND title LIKE %s') % ('%' + label + '%','%' + title + '%')
+        try:
+            query = db_insert('INSERT INTO store_mappings (release_id,store,store_url,unique_key,store_release_id) SELECT id,%s,%s, md5(concat(id,%s)),%s FROM releases_all WHERE label_no_country LIKE %s AND title LIKE %s ON DUPLICATE KEY UPDATE store_url=values(store_url),store_release_id=values(store_release_id)', ('rushhour','/' + release_url,'rushhour',store_release_id,label + '%','%' + title + '%'))
+            data = query.fetchall()
+            print(query,data)
+        except Exception as e:
+            print(str(e))
+            continue
         
     return base_url,201
 
